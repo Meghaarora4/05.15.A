17f70201b24fdaa07a8efa56c44072bdb05ecb97
Megha Arora <739680@student.tor.loc>
2019-05-15 11:56:17 -0400

    initial

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+            public static int GetSmallest(List<int> list)
+
+        {
+            var min = list[0];
+            for (var i = 1; i < list.Count; )
+        }
+    }
+}
